ced53203b42d14cc9ea046b66eca73338a39b8c9
ExpressGen24 <158236679+ExpressGen24@users.noreply.github.com>
2026-02-03 08:11:47 -0600

    floder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#create directory structure
+mkdir -p src
+mkdir -p research
+
+#creating files
+
+touch src/_init.py
+touch src/help.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory structure and files created successfully."
